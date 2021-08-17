aa8964ab688f266e8d5d5c5ee1734fef06d3c8bb
Sponch <sponch05>
2021-08-17 15:04:37 +0200

    Update code as per lessons

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { Router, RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+       
+        { path: 'welcome', component: WelcomeComponent },
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+        { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+
+      ]
+    ),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ APM-Start/src/app/shared/star.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
+
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+
+export class StarComponent implements OnChanges {
+
+    // allows us to recieve from parent
+    @Input() rating: number = 0;
+    cropWidth: number = 75;
+
+    @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+
+    ngOnChanges(): void {
+        this.cropWidth = this.rating * 75 / 5;
+    }
+
+
+    onClick(): void {
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked`)
+    }
+}
